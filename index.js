4f65c6c4fb86a321767090c31b54143945d90e81
karteekkr97 <62058343+karteekkr97@users.noreply.github.com>
2021-01-21 08:59:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ function validate() {
     var choice2 = document.getElementById("num1").checked;
     if ((choice1 == " ") && (choice2 == " ")) {
         alert("Choose atleast one item to unmark")
-        return false;
     }
-    return true
+    return true;
+}
+
+
+function addItem() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var li = document.createElement("li");
+    li.setAttribute('id', candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+}
+
+
+function removeItem() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("candidate");
+    var item = document.getElementById(candidate.value);
+    ul.removeChild(item);
 }
